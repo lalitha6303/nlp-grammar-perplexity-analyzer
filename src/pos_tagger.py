a8d5191621eb661753acf61de5c13b7d2855d81a
lalitha <lalithalakshmigorrela@gmail.com>
2026-02-04 08:23:24 +0530

    Fix NLTK POS tagger LookupError for Streamlit Cloud

--- src/pos_tagger.py
+++ src/pos_tagger.py
@@ -1,6 +1,8 @@
 import nltk
 
-def pos_tag(sentence):
-    nltk.download("averaged_perceptron_tagger", quiet=True)
-    tokens = sentence.split()
+# Download required NLTK data (only once)
+nltk.download("averaged_perceptron_tagger", quiet=True)
+nltk.download("punkt", quiet=True)
+
+def pos_tag(tokens):
     return nltk.pos_tag(tokens)
